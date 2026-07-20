c57fd6a8fb5f3203cce6b89f186f9c6853aa8e8f
adrian vigdal <vigdal@vigdal.dev>
2026-07-20 12:22:02 +0200

    Update binary and cache files for VigdalsMorningsguide and rjsmrazor projects
    - Updated VigdalsMorningsguide.dll to reflect changes in the build.
    - Modified rjsmcshtml.dswa.cache.json and rjsmrazor.dswa.cache.json to include updated global properties and input hashes.

--- obj/Debug/net10.0/VigdalsMorningsguide.AssemblyInfo.cs
+++ obj/Debug/net10.0/VigdalsMorningsguide.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VigdalsMorningsguide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9157fee264dccf541176603ac386bab13907a963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005e4922b63b6017003ff1d8c6340b6a47e50cdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("VigdalsMorningsguide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VigdalsMorningsguide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
